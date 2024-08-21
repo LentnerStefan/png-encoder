841bfb3e1c158d4447af4feb1b1ad6c678eefe74
Stef <18282455+LentnerStefan@users.noreply.github.com>
2024-08-21 17:26:35 +0200

    fix: cleanup imports

--- cpp/react-native-png-encoder.cpp
+++ cpp/react-native-png-encoder.cpp
@@ -1,8 +1,8 @@
 #include "react-native-png-encoder.h"
 // sstream contains functions to manipulate strings in C++
 #include <sstream>
+#include <jsi/jsi.h>
 #include "fpng.h"
-#include "fpng.cpp"
 #include "MutableRawBuffer.h"
 
 // The namespace allows for syntactic sugar around the JSI objects. ex. call: jsi::Function instead of facebook::jsi::Function
@@ -10,10 +10,10 @@ using namespace facebook;
 
 // We get the runtime from the obj-c code and we create our native functions here
 void installSequel(jsi::Runtime& jsiRuntime) {
-    
+
     // Initialize fpng once, as stated in the doc: https://github.com/richgel999/fpng?tab=readme-ov-file#initialization
     fpng::fpng_init();
-    
+
     // jsi::Function::createFromHostFunction will create a JavaScript function based on a "host" (read C++) function
     auto multiply = jsi::Function::createFromHostFunction(
                                                           jsiRuntime, // JSI runtime instance
@@ -39,41 +39,41 @@ void installSequel(jsi::Runtime& jsiRuntime) {
                                                               size_t width = arguments[1].asNumber();
                                                               size_t height = arguments[2].asNumber();
                                                               jsi::ArrayBuffer rgbBuffer = rgbBufferObj.getArrayBuffer(runtime);
-                                                              
+
                                                               uint8_t* rgbData = rgbBuffer.data(runtime);
                                                               size_t dataSize = rgbBuffer.size(runtime);
-                                                              
+
                                                               // Check the buffer is of the right size
                                                               size_t channelCount = 3;
                                                               if (width * height * channelCount != dataSize) {
                                                                   throw jsi::JSError(runtime, "Invalid RGB buffer size");
                                                               }
-                                                              
-                                                              
+
+
                                                               // Convert to png in memory
                                                               std::vector<uint8_t> pngBuffer;
                                                               fpng::fpng_encode_image_to_memory(rgbData, width, height, channelCount, pngBuffer);
-                                                              
-                                                              
+
+
                                                               // Start conversion to return as an ArrayBuffer on JS Land
                                                               size_t pngSize = pngBuffer.size();
                                                               static constexpr auto ARRAYBUFFER_CACHE_PROP_NAME = "__pngArrayBufferCache";
-                                                              
+
                                                               if (!runtime.global().hasProperty(runtime, ARRAYBUFFER_CACHE_PROP_NAME)) {
                                                                   auto mutableBuffer = std::make_shared<vision::MutableRawBuffer>(pngSize);
                                                                   jsi::ArrayBuffer arrayBuffer(runtime, mutableBuffer);
                                                                   runtime.global().setProperty(runtime, ARRAYBUFFER_CACHE_PROP_NAME, std::move(arrayBuffer));
                                                               }
-                                                              
+
                                                               auto arrayBufferCache = runtime.global().getPropertyAsObject(runtime, ARRAYBUFFER_CACHE_PROP_NAME);
                                                               auto arrayBuffer = arrayBufferCache.getArrayBuffer(runtime);
-                                                              
+
                                                               if (arrayBuffer.size(runtime) != pngSize) {
                                                                   auto mutableBuffer = std::make_shared<vision::MutableRawBuffer>(pngSize);
                                                                   arrayBuffer = jsi::ArrayBuffer(runtime, mutableBuffer);
                                                                   runtime.global().setProperty(runtime, ARRAYBUFFER_CACHE_PROP_NAME, arrayBuffer);
                                                               }
-                                                              
+
                                                               memcpy(arrayBuffer.data(runtime), pngBuffer.data(), pngSize);
                                                               return arrayBuffer;
                                                           }
--- cpp/react-native-png-encoder.h
+++ cpp/react-native-png-encoder.h
@@ -1,7 +1,6 @@
 #ifndef PNGENCODER_H
 #define PNGENCODER_H
 
-#include <jsi/jsilib.h>
 #include <jsi/jsi.h>
 
 void installSequel(facebook::jsi::Runtime& jsiRuntime);
